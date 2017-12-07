1a089d9f444edd874d0abedf07907f29bf4955f4
Isaias Santos <isaikki@gmail.com>
2017-12-07 18:03:44 -0200

    Primeiras alterações

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DB;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,9 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $posts = DB::table('posts')
+            ->select('posts.id', 'posts.title', 'posts.message', 'users.name', 'posts.created_at')
+            ->join('users', 'users.id', 'posts.id_user')->orderBy('id', 'desc')->get();
+        return view('home', array('posts' => $posts));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,13 +5,41 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Início</div>
+                <div class="panel-heading">
+                    Início
+                    <form method="post" >
+                        <div class="row">
+                            <div class="col-md-12">
+                                <label for="title">Título</label>
+                                <input class="form-control" type="text" name="title" id="title"/>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <label for="message">Mensagem</label>
+                                <textarea class="form-control" name="message" id="message"></textarea>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <input type="button" class="btn btn-success btn-sm" value="Enviar" />
+                            </div>
+                        </div>
+                    </form>
+                </div>
                 <div class="panel-body">
                     @if (session('status'))
                         <div class="alert alert-success">
                             {{ session('status') }}
                         </div>
                     @endif
+                    @foreach($posts as $post)
+                        <div class="post">
+                            <div class="post-title">{{$post->title}}</div>
+                            <div class="post-message">{{$post->message}}</div>
+                            <div class="post-footer">{{$post->name}} - {{$post->created_at}}</div>
+                        </div>
+                    @endforeach
                 </div>
             </div>
         </div>
